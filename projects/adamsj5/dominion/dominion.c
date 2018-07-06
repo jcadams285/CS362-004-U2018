b3cc46abdf00e12c49d4603844bc8199c2a206b1
Jason Adams <jcadams0285@gmail.com>
2018-07-06 15:35:55 -0500

    Update dominion.c

--- projects/adamsj5/dominion/dominion.c
+++ projects/adamsj5/dominion/dominion.c
@@ -645,15 +645,11 @@ int getCost(int cardNumber)
 
 void playAdventure(struct gameState *state){
     int currentPlayer = whoseTurn(state);
-    int nextPlayer = currentPlayer + 1;
     int temphand[MAX_HAND];// moved above the if statement
     int drawntreasure=0;
     int cardDrawn;
     int z = 0;// this is the counter for the temp hand
 
-    if (nextPlayer > (state->numPlayers - 1)){
-      nextPlayer = 0;
-    }
     while(drawntreasure<2){
     if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
       shuffle(currentPlayer, state);
