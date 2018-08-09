d3f497659a9bd7300540ab25efd0a1716e80bb64
Jason Adams <adamsj5@flip3.engr.oregonstate.edu>
2018-08-08 17:16:46 -0700

    Files for assignment 5

--- projects/adamsj5/praticokDominion/cardtest1.c
+++ projects/adamsj5/praticokDominion/cardtest1.c
@@ -68,57 +68,10 @@ int main (int argc, char** argv) {
 
     //Test 3- handcount should have two added treasure cards and 1 less kingdom card
     if (state.handCount[player] == (stateOrg.handCount[player] + 2 - 1)){
-        printf("Test 3: PASSED-- Correct number of cards were added to and played from handcount\n");
+        printf("Test 3: PASSED-- Correct number of cards were added to and played from handcount\n\n");
     }
     else{
-        printf("Test 3: FAILED-- Incorrect number of cards were added to and played from handcount\n");
-    }
-
-    //Test 4- Original deckCount - current deckCount should equal discardCount - orginal discardCount + 2(treasure cards kept - 1 discard of adventure card)
-    if (stateOrg.deckCount[player] - state.deckCount[player] == (state.discardCount[player] - stateOrg.discardCount[player] + 2)){
-        printf("Test 4: PASSED-- Number of cards discarded equals the number of cards drawn - 2\n");
-    }
-    else{
-        printf("Test 4: FAILED-- Number of cards discarded does not equal the number of cards drawn\n");
-    }
-
-
-        //Start up game with two players
-    initializeGame(numPlayers, k, randomSeed, &state);
-
-    //Copy state and store in stateOrg to check after test
-    stateOrg = state;
-
-    //add silver and kingdom cards to the deck 
-    state.deckCount[player] = 10;
-    for (int i = 0; i < 10; i++){
-      if (i % 2 == 0){
-        state.deck[player][i] = silver;
-      }
-      else{
-        state.deck[player][i] = mine;
-      }
-    }
-
-    //Copy state and store in stateOrg to check after test
-    stateOrg = state;
-
-    cardEffect(adventurer, choice1, choice2, choice3, &state, handPos, &bonus);
-
-    //Test 3- handcount should have two added treasure cards and 1 less kingdom card
-    if (state.handCount[player] == (stateOrg.handCount[player] + 2 - 1)){
-        printf("Test 5: PASSED-- Correct number of cards were added to and played from handcount\n");
-    }
-    else{
-        printf("Test 5: FAILED-- Incorrect number of cards were added to and played from handcount\n");
-    }
-
-    //Test 4- Original deckCount - current deckCount should equal discardCount - orginal discardCount + 2(treasure cards kept - 1 discard of adventure card)
-    if (stateOrg.deckCount[player] - state.deckCount[player] == (state.discardCount[player] - stateOrg.discardCount[player] + 2)){
-        printf("Test 6: PASSED-- Number of cards discarded equals the number of cards drawn - 2\n\n");
-    }
-    else{
-        printf("Test 6: FAILED-- Number of cards discarded does not equal the number of cards drawn\n\n");
+        printf("Test 3: FAILED-- Incorrect number of cards were added to and played from handcount\n\n");
     }
 
     return 0;
